323e07753a31d6ee4cd50f63bcc39e728f294a10
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-27 18:18:23 +0600

    Install Shadcn Components;

--- src/components/Todo/todoContainer.tsx
+++ src/components/Todo/todoContainer.tsx
@@ -1,16 +1,20 @@
+import { Button } from "../ui/button";
 import TodoCard from "./todoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <Button className="bg-primary-gradient text-xl font-semibold">Add todo</Button>
+        <Button>Filter</Button>
       </div>
-      <div className="bg-red-500 w-full  rounded-xl p-5 space-y-5">
-     <div className="bg-white text-2xl p-5 flex justify-center items-center font-bold">
-      <p>  There is no task pending</p>
-     </div>
+      <div className="bg-primary-gradient w-full  rounded-xl p-[5px] ">
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-5">
+            <TodoCard />
+        </div>
+        {/* <div className="bg-white text-2xl p-5 flex justify-center items-center font-bold">
+          <p> There is no task pending</p>
+        </div> */}
       </div>
     </div>
   );
